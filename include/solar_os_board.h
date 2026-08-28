fc00fb9de24aeacc1e851171d42a64f1acaea558
Marton Juhasz <marton@nowtech.hu>
2026-08-28 19:45:27 +0200

    Migrate Freenove WROVER board manifest

--- include/solar_os_board.h
+++ include/solar_os_board.h
@@ -8,8 +8,6 @@
 #include "boards/freenove_esp32_s3_display_4_0.h"
 #elif defined(SOLAR_OS_BOARD_ODROID_GO)
 #include "boards/odroid_go.h"
-#elif defined(SOLAR_OS_BOARD_FREENOVE_ESP32_WROVER_V3)
-#include "boards/freenove_esp32_wrover_v3.h"
 #elif defined(SOLAR_OS_BOARD_ELECROW_CROWPANEL_ESP32_S3_4_2_EPAPER)
 #include "boards/elecrow_crowpanel_esp32_s3_4_2_epaper.h"
 #elif defined(SOLAR_OS_BOARD_TTGO_VGA32_V14)
